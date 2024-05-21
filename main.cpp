741f1f31d92faee34628db2e593032ff2f4b5798
deamahfudz <37812053+littleschwan@users.noreply.github.com>
2024-05-21 19:56:02 +0700

    Fix switch cake

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <vector>
 #include <unordered_set>
+#include <algorithm>
 
 struct Plate {
     std::vector<int> cakes = { 0, 0, 0, 0, 0, 0 };
@@ -14,7 +15,7 @@ bool isFull(Plate input) {
     return true;
 }
 
-std::unordered_set<int> findSimilarNumbers(Plate a, Plate b) {
+std::unordered_set<int> findSimilarCakes(Plate a, Plate b) {
     std::unordered_set<int> numSet(a.cakes.begin(), a.cakes.end());
     std::unordered_set<int> similarNumbers;
 
@@ -29,26 +30,81 @@ std::unordered_set<int> findSimilarNumbers(Plate a, Plate b) {
 
 void switchCake(Plate a, Plate b) {
 
-    Plate current;
+    Plate current, neighbor;
+    int iterasi = 1;
 
-    while (!isFull(a) || !isFull(b) && findSimilarNumbers(a, b).empty() == false) {
+    std::cout << "Iterasi ke - 0: " << std::endl;
+    std::cout << "Current element   :" << std::endl;
+    for (int element : a.cakes) {
+        std::cout << element << " ";
+    }
+    std::cout << std::endl;
 
-        if (!isFull(a))
+    std::cout << "Neighbor element   :" << std::endl;
+    for (int element : b.cakes) {
+        std::cout << element << " ";
+    }
+    std::cout << std::endl;
+    std::cout << std::endl;
+
+    while ((!isFull(a) || !isFull(b))&& findSimilarCakes(a, b).empty() == false) {
+
+        std::unordered_set<int> similarCakes = findSimilarCakes(a, b);
+
+        std::cout << "Iterasi ke -" << iterasi << " :" << std::endl;
+
+        if (!isFull(a)) {
             current = a;
-        else
+            neighbor = b;
+        }
+        else {
             current = b;
+            neighbor = a;
+        }
+       
+        int temp = 0;
 
-        int i = 0;
+        std::sort(current.cakes.begin(), current.cakes.end());
+        std::sort(neighbor.cakes.begin(), neighbor.cakes.end());
 
-        for (int element : a.cakes) {
-            if (element != 0) break;
+        for (int i = 0; i < 6; i++) {
+            if (current.cakes[i] != 0)
+                break;
             else {
-
-
+                for (int j = 0; j < 6; j++) {
+                    if (similarCakes.find(neighbor.cakes[j]) != similarCakes.end()) {
+                        temp = neighbor.cakes[j];
+                        neighbor.cakes[j] = 0;
+                        current.cakes[i] = temp;
+                        break;
+                    }
+                }
             }
+        }
+       
+        a = current;
+        b = neighbor;
+        iterasi++;
 
+        std::cout << "Similar Cakes  :" << std::endl;
+        for (int element : similarCakes) {
+            std::cout << element << " ";
         }
 
+        std::cout << std::endl;
+
+        std::cout << "Current element   :" << std::endl;
+        for (int element : current.cakes) {
+            std::cout << element << " ";
+        }
+        std::cout << std::endl;
+
+        std::cout << "Neighbor element   :" << std::endl;
+        for (int element : neighbor.cakes) {
+            std::cout << element << " ";
+        }
+        std::cout << std::endl;
+        std::cout << std::endl;
     }
 }
 
@@ -59,15 +115,7 @@ int main() {
     Plates[0][1].cakes = { 1,1,2,2,3,3 };
     Plates[1][1].cakes = { 4,5,6,7,8,9 };
 
-    for (int element : findSimilarNumbers(Plates[0][1], Plates[0][0])) {
-        std::cout << element << " ";
-    }
-
-    std::cout << std::endl;
-
-    if (findSimilarNumbers(Plates[0][1], Plates[1][1]).empty() == true) {
-        std::cout << "Tidak ada angka yang sama" << std::endl;
-    }
+    switchCake(Plates[0][0], Plates[0][1]);
     return 0;
 };
 
